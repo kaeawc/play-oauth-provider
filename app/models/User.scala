fd99b2c712ca83d08a88865f538afea205fa906d
Jason Pearson <jason.d.pearson@gmail.com>
2013-12-02 10:07:20 -0500

    added implementation to User model

--- app/models/User.scala
+++ app/models/User.scala
@@ -1,8 +1,14 @@
 package models
 
-import java.util.Date
+import anorm._
+import anorm.SqlParser._
 
+import play.api.Logger
+import play.api.db.DB
+import play.api.Play.current
 import play.api.libs.json._
+
+import java.util.Date
 
 import scala.concurrent.{Future,ExecutionContext}
 
@@ -14,7 +20,15 @@ case class User(
   password : String,
   salt     : String,
   created  : Date
-)
+) {
+
+  def asPublic = Json.obj(
+    "id"      -> id,
+    "email"   -> email,
+    "created" -> created
+  )
+
+}
 
 object User extends ((
   Long,
@@ -29,24 +43,103 @@ with CRUD[User] {
 
   implicit val jsonFormat = Json.format[User]
 
-  def getById(id:Long) = ???
+  val users =
+    long("id") ~
+    str("email") ~
+    str("password") ~
+    str("salt") ~
+    date("created") map {
+      case   id~email~password~salt~created =>
+        User(id,email,password,salt,created)
+    }
 
-  def getByEmail(email:String) = Future { Some(User(1,"","","",new Date())) }
+  def getById(id:Long) = Future {
+    DB.withConnection { implicit connection =>
+      SQL(
+        """
+          SELECT
+            u.id,
+            u.email,
+            u.password,
+            u.salt,
+            u.created
+          FROM user u
+          WHERE id = {id};
+        """
+      ).on(
+        'id -> id
+      ).as(users.singleOpt)
+    }
+  }
 
-  def findByEmail(email:String) = ???
-
-  def create(user:User):Future[Option[User]] = create(user.email, user.password)
+  def getByEmail(email:String) = Future {
+    DB.withConnection { implicit connection =>
+      SQL(
+        """
+          SELECT
+            u.id,
+            u.email,
+            u.password,
+            u.salt,
+            u.created
+          FROM user u
+          WHERE email = {email};
+        """
+      ).on(
+        'email -> email
+      ).as(users.singleOpt)
+    }
+  }
 
   def create(email:String,password:String):Future[Option[User]] = {
-    User.getByEmail(email) map {
-      case Some(user:User) => None
+
+    val salt              = crypto.PBKDF2.createSalt()
+    val hashedPassword    = crypto.PBKDF2.hash(password,salt)
+    val storedSalt:String = salt
+    val created           = new Date()
+
+    getByEmail(email) map {
+      case Some(user:User) => {
+        Logger.warn("User already created")
+        None
+      }
       case _ => {
-
-        val salt = crypto.PBKDF2.createSalt()
-
-        val hashedPassword = crypto.PBKDF2.hash(password,salt)
-
-        Some(User(1,email,hashedPassword,salt,new Date()))
+        DB.withConnection { implicit connection =>
+          SQL(
+            """
+              INSERT INTO user (
+                email,
+                password,
+                salt,
+                created
+              ) VALUES (
+                {email},
+                {password},
+                {salt},
+                {created}
+              );
+            """
+          ).on(
+            'email    -> email,
+            'password -> hashedPassword,
+            'salt     -> storedSalt,
+            'created  -> created
+          ).executeInsert()
+        }
+      }
+    } map {
+      case Some(id:Long) => {
+        Some(User(
+          id,
+          email,
+          hashedPassword,
+          storedSalt,
+          created
+        ))
+      }
+      case _ => {
+        Logger.warn("User wasn't created")
+        None
       }
     }
   }
@@ -58,6 +151,7 @@ with CRUD[User] {
   def delete(id:Long,password:String) = ???
 
   def authenticate(email:String,password:String):Future[Option[User]] = {
+
     User.getByEmail(email) map {
       case Some(user:User) => {
 
